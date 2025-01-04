e484b349e421efd3a976663f8475852911dea9f7
asutoshp10 <135543227+asutoshp10@users.noreply.github.com>
2025-01-04 22:01:15 +0530

    Update and rename ok.py to notebook.py

--- notebook.py
+++ notebook.py
@@ -4,26 +4,9 @@ import os
 import tensorflow as tf
 import keras
 
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# if gpus:
-#     print('it is')
-#     try:
-#         tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-#         tf.config.experimental.set_memory_growth(gpus[0], True)
-#         print("Using GPU:", gpus[0])
-#     except RuntimeError as e:
-#         print(e)
-model=keras.models.load_model(r'C:\Users\asuto\Desktop\sld\obj_dect_x.h5',)
-img=r'C:\Users\asuto\Desktop\AI proj\Yolov1_Cyborg\Dataset\bk_img\bk24.png'
-cap=cv2.VideoCapture(r'now.mp4')
-# cap=cv2.VideoCapture(1)
-# for i in range(4):
-#     cap = cv2.VideoCapture(i)
-#     if cap.isOpened():
-#         print(f"Device {i} is available.")
-#         cap.release()
-#     else:
-#         print(f"Device {i} is not available.")
+model=keras.models.load_model(r'obj_dect_x.h5',)
+cap=cv2.VideoCapture(1)
+
 def empty(x):
     pass
 cv2.namedWindow('hsv')
@@ -35,7 +18,6 @@ def get_bbox(canny,frame_copy):
     cont,f=cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in cont:
         if cv2.contourArea(cnt)>1000:
-            # cv2.drawContours(frame_copy,cnt,-1,(0,255,0),5)
             peri=cv2.arcLength(cnt,True)
             approx=cv2.approxPolyDP(cnt,0.02*peri,True)
             x_,y_,w,h=cv2.boundingRect(approx)
@@ -45,21 +27,13 @@ def get_bbox(canny,frame_copy):
             li=['capacitor','data','IC','resistor','transistor']
             x=li[np.argmax(pred)]
             cv2.putText(frame_copy,x,(y_+h-5,x_),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-            # try:
-            #     cv2.imshow('p_img',p_img)
-            # except Exception as e:
-            #     print(e)
 
 while True:
     ret,frame=cap.read()
     print(frame.shape)
     frame_copy=frame.copy()
-    # cv2.imshow('frame',cv2.cvtColor(frame,cv2.COLOR_BGR2HSV))
     a=cv2.getTrackbarPos('1','hsv')
     b=cv2.getTrackbarPos('2','hsv')
-    # blur=cv2.GaussianBlur(frame,(7,7),1)
-    # hsv=cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
-    # blur2=cv2.bilateralFilter(hsv,3,35,35)
     blur=cv2.bilateralFilter(frame,3,35,35)
     gray=cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
     canny=cv2.Canny(blur,a,b)
